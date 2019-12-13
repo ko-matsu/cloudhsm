0957c7d7025c3fb2584b1f2379a1b68d229f3059
ko-matsu <tangrambs803@gmail.com>
2019-12-14 00:31:03 +0900

    github actions

---
+++ .github/workflows/docker/alpine_build_entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -l
+
+apk add --update --no-cache musl gcc g++ make git cmake
+
+cd /github/workspace
+ls
+
+mkdir -p dist/usr/local/lib64
+cd cloudhsm
+mkdir build
+cd build
+cmake --version
+cmake .. -DENABLE_SHARED=on -DCMAKE_BUILD_TYPE=Release -DTARGET_RPATH="/usr/local/lib;/usr/local/lib64"
+make
+cp ./Release/libcloudhsm.* /github/workspace/dist/usr/local/lib64
+ls /github/workspace/dist
+ls /github/workspace/dist/usr
+ls /github/workspace/dist/usr/local
+ls /github/workspace/dist/usr/local/lib64

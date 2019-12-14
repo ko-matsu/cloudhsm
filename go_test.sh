2009d52a7d54ada5c7d47e5ceab2c315d62bef72
ko-matsu <tangrambs803@gmail.com>
2019-12-14 20:59:10 +0900

    add mac test

--- go_test.sh
+++ go_test.sh
@@ -1,4 +1,4 @@
 if [ -z "$GO_EXEC_PATH" ]; then
 GO_EXEC_PATH=go
 fi
-LD_LIBRARY_PATH=./build/Release $GO_EXEC_PATH test
+LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./build/src/pkcs11:/usr/lib:/usr/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu/engines-1.1:/usr/lib/x86_64-linux-gnu/pkcs11" $GO_EXEC_PATH test
